af199d5a3b3426091bda26ecf226d1e9d0bfc25a
Nuno Gervasio <nunogee@gmail.com>
2019-05-27 10:49:18 +0200

    add snapshot test to <App />

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders properly", () => {
+  const app = shallow(<App />);
+  expect(app).toMatchSnapshot();
 });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
