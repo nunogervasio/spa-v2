ce46a0c345d3af100e616a086a591600f3ae0830
Nuno Gervasio <nunogee@gmail.com>
2019-05-28 10:37:26 +0200

    add headder folder and component

---
+++ src/Header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { Container, Section } from "../components";
+
+const Header = () => {
+  return (
+    <Section>
+      <Container>
+        <h1>HEADER</h1>
+      </Container>
+    </Section>
+  );
+};
+
+export default Header;
