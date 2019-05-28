9cac77161702ceca87fe6cab7177a285aa873796
Nuno Gervasio <nunogee@gmail.com>
2019-05-28 09:09:30 +0200

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import AboutUs from "./AboutUs/aboutUs";
 import Feature from "./Feature/feature";
+import Footer from "./Footer/footer";
 import "./App.css";
+import DemoFooter from "./DemoFooter/demoFooter";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <AboutUs />
       <Feature />
       <Feature swap={true} />
+      <Footer />
     </div>
   );
 }
