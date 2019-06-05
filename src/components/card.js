80ebdea87dc6d9345c9ab0fa41717aea9aaa4913
Nuno Gervasio <nunogee@gmail.com>
2019-06-05 14:43:48 +0200

    add box-shadow and responsiveness to cards in team section

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,6 @@ import { Button } from "./button";
 import { Paragraph } from "./paragraph";
 import { Image } from "./image";
 import { Title } from "./title";
-import yogi from "../images/t1.jpg";
 
 const CardContainer = styled.div`
   display: flex;
@@ -14,20 +13,34 @@ const CardContainer = styled.div`
   flex-grow: 1;
   ${"" /* max-width: 1100px; */}
   margin: 0.75em;
-  padding: 0.5em;
+  padding: 1em;
   background-color: white;
-  border-radius: 2%;
+  box-shadow: 0 0 15px -3px rgba(0, 0, 0, 0.2);
+  max-width: 330px;
+  height: 355px;
 `;
 
 const EmpImage = styled(Image)`
   border-radius: 50%;
 `;
-export const Card = () => {
+const RoleTitle = styled(Title)`
+  font-size: 1rem;
+  color: black;
+  margin-top: 0;
+`;
+const ProfileName = styled(Title)`
+  font-size: 1.2rem;
+  margin-bottom: 0.7rem;
+  color: midnightblue;
+`;
+
+export const Card = ({ img, name, title }) => {
   return (
     <CardContainer>
       {/* <Image src="https://via.placeholder.com/260" /> */}
-      <EmpImage src={yogi} />
-      <Title>Default Title</Title>
+      <EmpImage src={img} />
+      <ProfileName>{name}</ProfileName>
+      <RoleTitle>{title}</RoleTitle>
       <Paragraph>
         Lorem ipsum dolor sit amet consectetur adipiscing elit.
       </Paragraph>
