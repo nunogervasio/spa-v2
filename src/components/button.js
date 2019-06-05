2c519a87ba8c466771b3f7a213ae3036f3b539ac
Nuno Gervasio <nunogee@gmail.com>
2019-06-05 09:18:22 +0200

    removed button margin

--- src/components/button.js
+++ src/components/button.js
@@ -5,7 +5,7 @@ export const Button = styled.button`
   color: ${props => (props.primary ? "white" : "cornflowerblue")};
 
   font-size: 1em;
-  margin: 1em;
+  ${"" /* margin: 1em; */}
   padding: 0.25em 1em;
   border: 2px solid gainsboro;
   border-radius: 3px;
